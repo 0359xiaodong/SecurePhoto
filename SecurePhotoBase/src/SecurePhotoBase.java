327c66e21f3b1fb8f6c94d4122396478d4966270
U-enx-laptop\enx <enx@enx-laptop.(none)>
2011-11-24 12:20:28 +0000

    Initial base

---
+++ SecurePhotoBase/src/SecurePhotoBase.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: enx
+ * Date: 20.11.11
+ * Time: 18:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class SecurePhotoBase {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
