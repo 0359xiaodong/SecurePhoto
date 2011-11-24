258a3e66fcf226d6f27d5d1cbb5034ed48db1810
unknown <enx@enx-laptop.(none)>
2011-11-24 19:32:36 +0000

    Initial version of constants

---
+++ SecurePhotoCommon/src/eu/tpmusielak/securephoto/SPConstants.java
@@ -0,0 +1,10 @@
+package eu.tpmusielak.securephoto;
+
+/**
+ * File containing all global constants used throughout SecurePhoto project
+ */
+public class SPConstants {
+    public static final int SERVER_DEFAULT_PORT = 5535;
+
+    public static final String SERVER_OK_MESSAGE = "OK";
+}
